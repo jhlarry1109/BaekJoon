8402643e5ba8565d4823a3cae49d216bb19210b4
jhlarry1109 <jhlarry1109@gmail.com>
2025-06-30 08:18:14 +0900

    [Bronze IV] Title: 세수정렬, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -0,0 +1,16 @@
+a,b,c = map(int,input().split())
+if a < b and a < c:
+    if b < c:
+        print(a,b,c)
+    else:
+        print(a,c,b)
+elif b < a and b < c:
+    if a < c:
+        print(b,a,c)
+    else:
+        print(b,c,a)
+else:
+    if b < a:
+        print(c,b,a)
+    else:
+        print(c,a,b)

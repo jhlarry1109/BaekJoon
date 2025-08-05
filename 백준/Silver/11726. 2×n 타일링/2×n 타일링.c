f803cac31ed4654c53fbb40035831f6993791b48
jhlarry1109 <jhlarry1109@gmail.com>
2025-08-05 19:28:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int li[1001];
+    li[0] = 1;
+    li[1] = 1;
+    for(int i=2; i<=n; i++) {
+        li[i] = (li[i-2] + li[i-1])%10007;
+    }
+    printf("%d", li[n]);
+    return 0;
+}

223f39c09f12c7fb97893e82daf6648a3f36b60b
jhlarry1109 <jhlarry1109@gmail.com>
2025-08-11 15:35:57 +0900

    [Gold II] Title: 문제집, Time: 2624 ms, Memory: 42936 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,23 @@
+import heapq
+
+n,m = map(int,input().split())
+indegree = [0] * (n+1)
+graph = {i:[] for i in range(1,n+1)}
+
+for i in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+heap = []
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        heapq.heappush(heap, i)
+        
+while heap:
+    node = heapq.heappop(heap)
+    print(node, end=" ")
+    for i in graph[node]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            heapq.heappush(heap, i)

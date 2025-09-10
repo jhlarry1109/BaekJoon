8a489ce6001e95754f5566b66f0678d131b02043
jhlarry1109 <jhlarry1109@gmail.com>
2025-09-10 09:52:43 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 4608 ms, Memory: 269068 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,29 +1,38 @@
-import heapq
+def find(x):
+    if parents[x] < 0:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
 
-n,m = map(int,input().split())
-graph = {i:[] for i in range(1,n+1)}
-visited = {i: False for i in range(1,n+1)}
-cost = 0
-big = 0
+def union(x, y):
+    x_root = find(x)
+    y_root = find(y)
+    if x_root == y_root:
+        return False
+    if parents[x_root] < parents[y_root]:
+        parents[x_root] += parents[y_root]
+        parents[y_root] = x_root
+    else:
+        parents[y_root] += parents[x_root]
+        parents[x_root] = y_root
+    return True
+
+n, m = map(int, input().split())
+li = []
+
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((c,b))
-    graph[b].append((c,a))
+    a, b, c = map(int, input().split())
+    li.append((c, a, b))
 
+li.sort()
 
-pq = []
-visited[1] = True
+parents = [-1] * (n+1)
+total = 0
+cost = []
 
-for vertex in graph[1]:
-    heapq.heappush(pq, vertex)
+for c, a, b in li:
+    if union(a, b):
+        total += c
+        cost.append(c)
 
-while pq:
-    c, node = heapq.heappop(pq)
-    if not visited[node]:
-        cost += c
-        big = max(big, c)
-        visited[node] = True
-        for vertex in graph[node]:
-            heapq.heappush(pq, vertex)
-        
-print(cost - big)
+print(total - max(cost))

4185aad1bd535432a231626c50a3404e7d2b7311
jhlarry1109 <jhlarry1109@gmail.com>
2025-09-17 23:35:18 +0900

    [Bronze V] Title: WARBOY, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(c*3*b/a);
+    }
+}

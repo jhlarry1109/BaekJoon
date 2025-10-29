574bea87d9a87dba125e4efb274a37f23f523a02
jhlarry1109 <jhlarry1109@gmail.com>
2025-10-29 23:44:10 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+word = input().strip()
+
+if word == "SONGDO":
+    print("HIGHSCHOOL")
+elif word == "CODE":
+    print("MASTER")
+elif word == "2023":
+    print("0611")
+elif word == "ALGORITHM":
+    print("CONTEST")
